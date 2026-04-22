76c6e8041f86abc50cbdaa9e1f4e012de4484fa1
LiorSon <128977035+LiorSonK@users.noreply.github.com>
2026-04-22 20:53:18 +0300

    files
    # Conflicts:
    #	game_module/game/game_code.py
    #	game_module/game/game_constants.py

--- game_module/game/game_code.py
+++ game_module/game/game_code.py
@@ -1,53 +1,14 @@
-#imports
 import pygame
-from game_constants import *
-
-#functions
-
-def timer():
-    pass
-def print_board(board):
-    [print(i) for i in board]
-def draw_board(screen,board):
-    x = BOARD_X_START
-    y = BOARD_Y_START
-    xi = x
-    for i in board:
-        for j in i:
-            if j == '':
-                pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
-            if j == 'R':
-                pygame.draw.rect(screen, RED, (x, y, SQUARE_LEN, SQUARE_LEN))
-                pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
-            if j == 'G':
-                pygame.draw.rect(screen, GREEN, (x, y, SQUARE_LEN, SQUARE_LEN))
-                pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
-            if j == 'B':
-                pygame.draw.rect(screen, BLUE, (x, y, SQUARE_LEN, SQUARE_LEN))
-                pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
-            if j == 'Y':
-                pygame.draw.rect(screen, YELLOW, (x, y, SQUARE_LEN, SQUARE_LEN))
-                pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
-            x += SQUARE_LEN
-        y += SQUARE_LEN
-        x= xi
-
-#initialize
 
 pygame.init()
 
-screen = pygame.display.set_mode((SCREEN_X,  SCREEN_Y))
+screen = pygame.display.set_mode((640,  640))
 
 running = True
 
-board = [[''] * BOARD_X_LEN for i in range(BOARD_Y_LEN)]
-
-#game loop
 while running:
-    screen.fill(WHITE)
-    draw_board(screen,board)
-    pygame.display.flip()
-    #close the game when the X button/ESC key is pressed
+
+    #close the game when the X button is pressed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
