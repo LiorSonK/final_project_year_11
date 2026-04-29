9f435149fb46048ac92f9e9899fa29657da2b2fb
LiorSon <128977035+LiorSonK@users.noreply.github.com>
2026-04-29 16:07:16 +0300

    added statistics, changed constants , added the option to show the player his cube

--- game_module/game/game_code.py
+++ game_module/game/game_code.py
@@ -1,9 +1,42 @@
 #imports
 import pygame
 from game_constants import *
-
+import random
 #functions
+def count_board(board):
+    r,g,b,y=0,0,0,0
+    for i in board:
+        for j in i:
+            if j =='R' or j == 'r':
+                r+=1
+            if j =='G' or j == 'g':
+                g+=1
+            if j =='B' or j == 'b':
+                b+=1
+            if j =='Y' or j == 'y':
+                y+=1
+    return r,g,b,y
+def draw_statistics(rgby):
+    title_font = pygame.font.SysFont(None,TITLE_SIZE)
+    title_text = title_font.render("STATISTICS",True,BLACK)
 
+    statistics_font = pygame.font.SysFont(None, STATISTIC_SIZE)
+    total_colored = sum(rgby)
+    total_left = (BOARD_X_LEN*BOARD_Y_LEN) - total_colored
+
+    screen.blit(statistics_font.render(f'The color red has: {rgby[0]}',True,RED),
+                (TITLE_X,TITLE_Y + STATISTIC_Y_OFFSET))
+    screen.blit(statistics_font.render(f'The color green has: {rgby[1]}', True, GREEN),
+                (TITLE_X, TITLE_Y + STATISTIC_Y_OFFSET*1.5))
+    screen.blit(statistics_font.render(f'The color blue has: {rgby[2]}', True, BLUE),
+                (TITLE_X, TITLE_Y + STATISTIC_Y_OFFSET*2))
+    screen.blit(statistics_font.render(f'The color yellow has: {rgby[3]}', True, YELLOW),
+                (TITLE_X, TITLE_Y + STATISTIC_Y_OFFSET*2.5))
+    screen.blit(statistics_font.render(f'total colored: {total_colored}', True, BLACK),
+                (TITLE_X, TITLE_Y + STATISTIC_Y_OFFSET * 3))
+    screen.blit(statistics_font.render(f'total left: {total_left}', True, BLACK),
+                (TITLE_X, TITLE_Y + STATISTIC_Y_OFFSET * 3.5))
+    screen.blit(title_text,(TITLE_X,TITLE_Y))
 def timer():
     pass
 def print_board(board):
@@ -12,8 +45,11 @@ def draw_board(screen,board):
     x = BOARD_X_START
     y = BOARD_Y_START
     xi = x
+    font = pygame.font.SysFont(None,PLAYER_TEXT_SIZE)
+    text = font.render("U",True, BLACK)
     for i in board:
         for j in i:
+            #land cubes
             if j == '':
                 pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
             if j == 'R':
@@ -28,6 +64,39 @@ def draw_board(screen,board):
             if j == 'Y':
                 pygame.draw.rect(screen, YELLOW, (x, y, SQUARE_LEN, SQUARE_LEN))
                 pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
+            #player cube
+            if j == 'r':
+                if color == 'R':
+                    pygame.draw.rect(screen, RED, (x, y, SQUARE_LEN, SQUARE_LEN))
+                    screen.blit(text,(x+PLAYER_TEXT_OFFSET_X,y+PLAYER_TEXT_OFFSET_Y))
+                    pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
+                else:
+                    pygame.draw.rect(screen, RED, (x, y, SQUARE_LEN, SQUARE_LEN))
+                    pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
+            if j == 'g':
+                if color =='G':
+                    pygame.draw.rect(screen, GREEN, (x, y, SQUARE_LEN, SQUARE_LEN))
+                    screen.blit(text, (x + PLAYER_TEXT_OFFSET_X, y + PLAYER_TEXT_OFFSET_Y))
+                    pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
+                else:
+                    pygame.draw.rect(screen, GREEN, (x, y, SQUARE_LEN, SQUARE_LEN))
+                    pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
+            if j == 'b':
+                if color=='B':
+                    pygame.draw.rect(screen, BLUE, (x, y, SQUARE_LEN, SQUARE_LEN))
+                    screen.blit(text, (x + PLAYER_TEXT_OFFSET_X, y + PLAYER_TEXT_OFFSET_Y))
+                    pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
+                else:
+                    pygame.draw.rect(screen, BLUE, (x, y, SQUARE_LEN, SQUARE_LEN))
+                    pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
+            if j == 'y':
+                if color=='Y':
+                    pygame.draw.rect(screen, YELLOW, (x, y, SQUARE_LEN, SQUARE_LEN))
+                    screen.blit(text, (x + PLAYER_TEXT_OFFSET_X, y + PLAYER_TEXT_OFFSET_Y))
+                    pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
+                else:
+                    pygame.draw.rect(screen, YELLOW, (x, y, SQUARE_LEN, SQUARE_LEN))
+                    pygame.draw.rect(screen, BLACK, (x, y, SQUARE_LEN, SQUARE_LEN), BORDER_WIDTH)
             x += SQUARE_LEN
         y += SQUARE_LEN
         x= xi
@@ -42,10 +111,14 @@ running = True
 
 board = [[''] * BOARD_X_LEN for i in range(BOARD_Y_LEN)]
 
+color = 'R'
+
+cr = ['R','G','B','Y']
 #game loop
 while running:
     screen.fill(WHITE)
     draw_board(screen,board)
+    draw_statistics(count_board(board))
     pygame.display.flip()
     #close the game when the X button/ESC key is pressed
     for event in pygame.event.get():
@@ -54,5 +127,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+            if event.key == pygame.K_SPACE:
+                board[random.randint(1,29)][random.randint(1,29)] = cr[random.randint(0,3)]
 
 pygame.quit()
